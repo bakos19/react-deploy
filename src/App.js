33c5d012aeb48a2a2df029f92ecf6d5ea3524805
bakos19 <diakiteaboubecrine@gmail.com>
2025-04-03 12:56:47 +0000

    code source

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import tehlilLogo from "./tehlil.jpeg"
+
+function App() {
+  return (
+    <div className="App">
+      <img src={tehlilLogo} alt="tehlil" />
+      <h1>Plateforme du notaire</h1>
+    </div>
+  );
+}
+
+export default App;
